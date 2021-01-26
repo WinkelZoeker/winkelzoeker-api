402e132c4d589587fc738eefd341fc93e8567099
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-26 22:25:05 +0000

    Docker related files

--- bin/build-winkelzoeker-api.sh
+++ bin/build-winkelzoeker-api.sh
@@ -4,6 +4,6 @@ set -e
 
 echo 'Generating WinkelZoeker-api docker image'
 
-docker build -t winkelzoeker/winkelzoeker-api -f ../Dockerfile.yml .
+docker build -t winkelzoeker/winkelzoeker-api -f Dockerfile.yml .
 
 echo 'Image built successfully.'
