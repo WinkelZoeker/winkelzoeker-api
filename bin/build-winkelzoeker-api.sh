43dc8cb79560a6e27c3e94ad1b471d0e8b2e8902
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-27 21:10:37 +0000

    Image generation - WIP

--- bin/build-winkelzoeker-api.sh
+++ bin/build-winkelzoeker-api.sh
@@ -5,5 +5,6 @@ set -e
 echo 'Generating WinkelZoeker-api docker image'
 
 docker build -t winkelzoeker/winkelzoeker-api -f Dockerfile.yml .
+# docker build -t winkelzoeker/winkelzoeker-api -f Dockerfile-GHA.yml .
 
 echo 'Image built successfully.'
