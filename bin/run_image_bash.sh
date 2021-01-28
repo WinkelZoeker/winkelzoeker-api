5f18c91989cf632a9aa5c609ceb4ddaf07619d2b
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-28 17:09:51 +0000

    Run docker image standalone

--- bin/run_image_bash.sh
+++ bin/run_image_bash.sh
@@ -4,4 +4,9 @@ set -e
 
 IMAGE='docker.pkg.github.com/winkelzoeker/winkelzoeker-api/winkelzoeker.api:head'
 
-docker run -it --entrypoint /bin/sh $IMAGE
+
+# REGULAR START
+docker run -it -p 3000:3000 --env-file ./secrets/.DEV.docker.env $IMAGE
+
+# WITH SHELL
+# docker run -it --env-file ./secrets/.DEV.docker.env --entrypoint /bin/sh $IMAGE
