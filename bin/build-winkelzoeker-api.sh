064fbf803f4dbdf6a304fb47cbf9e53d34f4c2f0
Rodrigo de Souza <rsouza01@gmail.com>
2021-02-01 20:41:07 +0000

    Removing unused files

--- bin/build-winkelzoeker-api.sh
+++ bin/build-winkelzoeker-api.sh
@@ -5,6 +5,5 @@ set -e
 echo 'Generating WinkelZoeker-api docker image'
 
 docker build -t winkelzoeker/winkelzoeker-api -f Dockerfile.yml .
-# docker build -t winkelzoeker/winkelzoeker-api -f Dockerfile-GHA.yml .
 
 echo 'Image built successfully.'
--- bin/stack-down.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# Set the environment (Database, MQTT, etc)
-echo -ne "Stoping docker-compose stack..." 
-docker-compose -f docker-compose.yml down
-echo "done!" 
