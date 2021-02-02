e78951e75caa454ce81a9323d3d1b6cf6db60464
Rodrigo de Souza <rsouza01@gmail.com>
2021-02-02 22:51:18 +0000

    Commented code removed

--- src/usecases/stores/findNearestStoresUseCase.ts
+++ src/usecases/stores/findNearestStoresUseCase.ts
@@ -38,34 +38,4 @@ export default class FindNearestStoresUseCase implements UseCase {
 			}, error);
 		}
 	}
-	
-	// async execute(input: UseCaseRequest): Promise<Store[]> {
-	// 	try {
-	// 		const mappedRecords = await this.storeRepository.findAll();
-
-	// 		if (input.hasOwnProperty('geoLocation')) {
-	// 			const mappedRecordsWithDistance = mappedRecords.map((record: Store) => {
-	// 				const distanceKm: number =
-	// 					distanceInMeters(
-	// 						input.geoLocation,
-	// 						new GeoLocation(record.latitude, record.longitude)) / 1000;
-	// 				return { ...record, distanceKm };
-	// 			})
-	// 				.sort((storeA, storeB) => storeA.distanceKm > storeB.distanceKm ? 1 : -1)
-	// 				.slice(0, input.limit);
-
-	// 			return mappedRecordsWithDistance;
-	// 		}
-	// 		return mappedRecords.slice(0, input.limit);
-	// 	} catch (error) {
-	// 		throw new UseCaseError({
-	// 			code: '1', /**  BUSINESS ERROR CODE, MAPPED SOMEWHERE, LIKE CONFLUENCE... */
-	// 			message: 'An error ocurred while fetching/mapping the results from FindNearestStoresUseCase.execute',
-	// 			severity: ErrorSeverity.ERROR,
-	// 			error
-	// 		}, error);
-	// 	}
-	// }
-
-
 }
